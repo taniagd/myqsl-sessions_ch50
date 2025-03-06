035a5f5d9f555c4c2b7863aa51d2f938056df3ee
Tania Gayosso <taniagd09@gmail.com>
2025-03-06 12:53:43 -0600

    documento de ejercicios de consultas

---
+++ ejercicio-consultas.sql
@@ -0,0 +1,10 @@
+-- Película de mayor duración
+-- Películas con título de más de 15 letras.
+-- ¿Cuántos clientes hay?
+-- Enlista todos los clientes activos, incluyendo su nombre completo y dirección de correo electrónico.
+-- Cuenta cuántos clientes han rentado alguna vez. Cuente a cada cliente una sola vez.
+-- ¿Cuántas películas hay en cada tienda?
+-- Enlista las categorías en orden alfabético.
+-- Buscar clientes con correo yahoo.com
+-- Buscar actores con apellidos Johnson y Doe.
+
