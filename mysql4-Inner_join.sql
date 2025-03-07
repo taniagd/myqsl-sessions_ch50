f76023770ade2a08b82a23b35188543ff2b648b7
Sergio Torres <sergio.torres@idr.edu.mx>
2025-03-07 11:16:58 -0600

    feat: inner join con where, order by

--- mysql4-Inner_join.sql
+++ mysql4-Inner_join.sql
@@ -1,7 +1,7 @@
 /*
  JOINS
  Se utiliza para combinar filas de dos o más tablas, basándose
- en una columna relaciona entre ellas.
+ en una columna relacional entre ellas.
  
  INNER JOIN: Devuelve registros cuando hay coinidencia en ambas tablas
  
@@ -19,6 +19,82 @@ USE tienda3b;
 SELECT * 
 	FROM products
     INNER JOIN categories
-    ON fk_category_id = category_id;
+    ON category_id = category_id;
 
+/* Ser explicito en le nombre de la tabla.campo */
+SELECT * 
+	FROM products
+    INNER JOIN categories
+    ON products.fk_category_id = categories.category_id;
 
+/* Formas de usar un alias en el nombre de la tabla */
+SELECT * 
+	FROM products AS p
+    INNER JOIN categories c
+    ON p.fk_category_id = c.category_id;
+
+/* Mostrar columnas especificas */
+SELECT 
+	p.product_id AS id,
+    p.name,
+    p.price,
+    c.name AS category_name
+	FROM products AS p
+    INNER JOIN categories AS c
+    ON p.fk_category_id = c.category_id;
+    
+/* Mostrar columnas especificas, ordenar por product name */
+SELECT 
+	p.product_id AS id,
+    p.name,
+    p.price,
+    c.name AS category_name
+	FROM products AS p
+    INNER JOIN categories AS c
+    ON p.fk_category_id = c.category_id
+    ORDER BY p.name;
+    
+/* Mostrar columnas especificas, ordenar por category name y price de forma descendente */
+SELECT 
+	p.product_id AS id,
+    p.name,
+    p.price,
+    c.name AS category_name
+	FROM products AS p
+    INNER JOIN categories AS c
+    ON p.fk_category_id = c.category_id
+    ORDER BY c.name DESC, p.price DESC;
+    
+/* - Mostrar columnas especificas, 
+   - ordenar por category name y price de forma descendente 
+   - Mostrar solo los productos mayores a 100 en price
+*/
+SELECT 
+	p.product_id AS id,
+    p.name,
+    p.price,
+    c.name AS category_name
+	FROM products AS p
+    INNER JOIN categories AS c
+    ON p.fk_category_id = c.category_id
+    -- filtrar
+    WHERE p.price >= 100
+    ORDER BY c.name DESC, p.price DESC;
+
+/* - Mostrar columnas especificas, 
+   - ordenar por category name y price de forma descendente 
+   - Mostrar solo los productos mayores a 100 en price
+   - Mostrar solo las categorias Moda y accesorios y linea blanca
+*/
+SELECT 
+	p.product_id AS id,
+    p.name,
+    p.price,
+    c.name AS category_name
+	FROM products AS p
+    INNER JOIN categories AS c
+    ON p.fk_category_id = c.category_id
+    -- filtrar
+    WHERE p.price >= 100 AND c.category_id IN (2,4) -- Linea blanca, moda y accesorios
+    ORDER BY c.name DESC, p.price DESC;
+    
---
+++ mysql4-ejercicio-inner join users and purchases.sql
@@ -0,0 +1,9 @@
+SELECT * FROM users;
+SELECT * FROM purchases;
+
+/* - Mostrar columnas: email del usuario, fecha de compra(purchase_date)  
+   - Ordenar por email de forma descendente 
+   
+   Se mostrarian solamentew los usuarios que han hecho uan compra
+   y la fecha e que la realizaron.
+*/
